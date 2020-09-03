5fbe17b599a6a26ab584cda30386e3d61ee69bc1
kingo2157 <kingo2157@users.noreply.github.com>
2020-09-03 20:28:57 +0100

    JS Text in a span, changed container to p

--- static/js/home.js
+++ static/js/home.js
@@ -1,4 +1,4 @@
 function downloaded()
 {
-    document.getElementById("description").textContent = "Share you're the best! highlights easily";
+    document.getElementById("changingText").textContent = " you're the best! ";
 }
