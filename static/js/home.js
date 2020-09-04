0d3f7d21631b3a4ab38d9b36921c83501187f1c0
kingo2157 <kingo2157@users.noreply.github.com>
2020-09-04 20:32:06 +0100

    JS Fix

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,5 @@
 let downloadBtn = document.getElementById("downloadButton");
 
-button.addEventListener("click", function() {
+downloadBtn.addEventListener("click", function() {
     document.getElementById("changingText").textContent = " you're the best! ";
 });
