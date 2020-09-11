47abbd2c7d427f46f0a18c31ed09129ab8d5d10b
IRHM <37304121+IRHM@users.noreply.github.com>
2020-09-11 20:51:51 +0100

    Format files

--- static/js/home.js
+++ static/js/home.js
@@ -1,16 +1,16 @@
 var downloadBtn = document.getElementById("downloadButton");
 var functionRan = false;
 
-downloadBtn.addEventListener("click", function() {
-    if (functionRan == false) {
-        document.getElementById("title").textContent = "Thanks!"
-        document.getElementById("description").textContent = "You're the best!"
+downloadBtn.addEventListener("click", function () {
+  if (functionRan == false) {
+    document.getElementById("title").textContent = "Thanks!"
+    document.getElementById("description").textContent = "You're the best!"
 
-        setTimeout(function() {
-            document.getElementById("title").textContent = "Casterr"
-            document.getElementById("description").textContent = "Share your best highlights easily"
-        }, 4000);
+    setTimeout(function () {
+      document.getElementById("title").textContent = "Casterr"
+      document.getElementById("description").textContent = "Share your best highlights easily"
+    }, 4000);
 
-        functionRan = true;  
-    }  
+    functionRan = true;
+  }
 });
