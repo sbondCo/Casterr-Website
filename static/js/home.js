021faea8a66e0849af304928da056416f0c1ba50
IRHM <37304121+IRHM@users.noreply.github.com>
2021-06-04 10:29:40 +0100

    Add button styling to _norm.scss and make downloads page more responsive

--- static/js/home.js
+++ static/js/home.js
@@ -1,16 +1,19 @@
 const downloadBtn = document.getElementById("downloadButton");
+const sourceBtn = document.getElementById("sourceButton");
 var hasFunctionRan = false;
 
 downloadBtn.addEventListener("click", function () {
   if (hasFunctionRan == false) {
     document.getElementById("title").textContent = "Thanks!";
-    document.getElementById("description").textContent = "You're the best!";
 
     setTimeout(function () {
       document.getElementById("title").textContent = "Casterr";
-      document.getElementById("description").textContent = "Share your best highlights easily";
     }, 4000);
 
     hasFunctionRan = true;
   }
 });
+
+// downloadBtn.addEventListener("click", function () {
+//   window.open();
+// });
