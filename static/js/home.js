65c4f303028dbd584f35cbe52af12e3ce51bbf3a
IRHM <37304121+IRHM@users.noreply.github.com>
2021-06-08 07:13:01 +0100

    Create setDownloadLinks() and add loader to button whilst fetching links

--- static/js/home.js
+++ static/js/home.js
@@ -1,15 +1,46 @@
 const downloadBtn = document.getElementById("downloadButton");
-var hasSaidThanks = false;
+let hasSaidThanks = false;
 
-downloadBtn.addEventListener("click", function () {
-  if (hasSaidThanks == false) {
+let winDownloadURL = "";
+let linuxDownloadURL = "";
+
+/**
+ * Get latest release from github repo and set download link vars.
+ */
+function setDownloadLinks() {
+  fetch("https://api.github.com/repos/sbondCo/Casterr/releases", {
+    method: "GET",
+    headers: {
+      Accept: "application/vnd.github.v3+json"
+    }
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      let assets = data[0].assets;
+
+      let win = assets.filter((a) => a.name.toLowerCase().endsWith(".exe"))[0];
+      let linux = assets.filter((a) => a.name.toLowerCase().endsWith(".appimage"))[0];
+
+      winDownloadURL = win.browser_download_url;
+      linuxDownloadURL = linux.browser_download_url;
+
+      downloadBtn.classList.remove("loading");
+
+      console.log(win, linux);
+    });
+}
+
+downloadBtn.addEventListener("click", () => {
+  if (!hasSaidThanks) {
     let toRevertTo = document.getElementById("title").textContent;
     document.getElementById("title").textContent = "Thanks!";
 
-    setTimeout(function () {
+    setTimeout(() => {
       document.getElementById("title").textContent = toRevertTo;
     }, 4000);
 
     hasSaidThanks = true;
   }
 });
+
+setDownloadLinks();
